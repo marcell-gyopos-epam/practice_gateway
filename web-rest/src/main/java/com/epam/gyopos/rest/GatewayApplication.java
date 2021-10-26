b7e8369d90e4fe319f9aa8eaa91bb51d618a0942
Marcell Gyöpös <marcell_gyopos@epam.com>
2021-10-26 15:11:20 +0200

    Modularize the project, add dependencies

--- web-rest/src/main/java/com/epam/gyopos/rest/GatewayApplication.java
+++ web-rest/src/main/java/com/epam/gyopos/rest/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.epam.gyopos.gateway;
+package com.epam.gyopos.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
